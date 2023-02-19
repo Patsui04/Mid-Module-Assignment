6e9873f962eebfab8c1b15faeab61a2d76714b64
Patrick <patrick@patricks-mbp.home>
2023-02-19 19:45:59 +0000

    Minor changes on the code

--- index.py
+++ index.py
@@ -1,36 +1,17 @@
 import sys
-import itertools
-
 NO_PATH = sys.maxsize
 
-graph = [[0, 7, NO_PATH, 8],
+input = [[0, 7, NO_PATH, 8],
          [NO_PATH, 0, 5, NO_PATH],
          [NO_PATH, NO_PATH, 0, 2],
          [NO_PATH, NO_PATH, NO_PATH, 0]]
 
-MAX_LENGTH = len(graph)
-
-
-# Not part of the requirements of the assignment but added for better visual of result
-
-
-def printSolution(dist):
-    print("Following matrix shows the shortest distances between every pair of vertices")
-    for i in range(MAX_LENGTH):
-        for j in range(MAX_LENGTH):
-            if (dist[i][j] == NO_PATH):
-                print("%7s" % ("NO_PATH"), end=" ")
-            else:
-                print("%7d\t" % (dist[i][j]), end=' ')
-            if j == MAX_LENGTH-1:
-                print()
-
 # Recursive version of the code
 
 
-def floydNew():
+def floydNew(graph):
     def shortestPath(i, j, k):
-        length = MAX_LENGTH-1
+        length = len(graph)-1
         if k < length:
             shortestPath(i, j, k + 1)
         if i < length:
@@ -41,7 +22,8 @@ def floydNew():
         graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
     shortestPath(0, 0, 0)
-    return printSolution(graph)
+    print(graph)
+    return graph
 
 
-floydNew()
+floydNew(input)
